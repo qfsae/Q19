85530b07ea25721c995bb22249909bb6b53efc00
Brent Champion <brent.champion@queensu.ca>
2019-03-29 19:03:01 -0400

    patches

--- Fuel_Controller/fuelControllerV1/fuelControllerV1.ino
+++ Fuel_Controller/fuelControllerV1/fuelControllerV1.ino
@@ -1,63 +1,118 @@
-//First testing for Q19 Fuel pump 
-// Need to figure out how to offset the PWM 
 #include <PID_v1.h>
 
-#define pressureReading A0
-#define output 3
-#define pressureUnderLED 1
-#define pumpingLED 2
 
-const int pumpThreshold = 400;
+const int pressureReading = A0; //Analog input from pressure sensor
+const int pumpPWM = 9; //PWM output from arduino
+
+//PWM output must have frequency >1000Hz as well as >2.5V Amplitude
+
+//for testing
+const int pressureUnderLED = 3;
 
 
-double input;
-double outputPWM;
+//to use in main loop
+int setPWM = 0;
+int currentPressure = 0;
 double pressureDifference;
 
+//PID Control Inputs
+double outputPWM;
+double inputPressure;
+double pressureThreshold = 400;
+
 //PID Tunings
-const double kp = 1;
+const double kp = 0;
 const double ki = 0.00;
 const double kd = 0.00;
 
-PID myPID(&input, &outputPWM, &pumpThreshold, kp, ki, kd, DIRECT);
+PID myPID(&inputPressure, &outputPWM, &pressureThreshold, kp, ki, kd, DIRECT);
 
 void setup(){
-  input = 0;
+  inputPressure = 0;
   pressureDifference = 0;
-
+  
+ setPWMFrequency(6, 8);
+  
   myPID.SetMode(AUTOMATIC);
-  myPID.SetSampleTime(10);
-
+  myPID.SetSampleTime(2);
+  
+  setPWM = 120; //resting PWM signal
+  analogWrite(pumpPWM, setPWM);
+  
   pinMode(pressureReading, INPUT);
-  pinMode(output, OUTPUT);
+  pinMode(pumpPWM, OUTPUT);
 
   pinMode(pressureUnderLED, OUTPUT);
-  pinMode(pumpingLED, OUTPUT);
+  digitalWrite(pressureUnderLED, LOW);
 
   Serial.begin(9600);
-  Serial.print("Desired Pressure\t Current Pressure\t Pressure Difference\n");
+  Serial.print("Desired Pressure\t Current Pressure\t Pressure Difference\t OutputPWM\n");
 }
 
 void loop(){
-  Serial.print(pumpThreshold);
+  Serial.print(pressureThreshold);
   Serial.print("\t");
   
-  currentPressure = constrain(analogRead(pressureReading));
-  Serial.print(currentPressure);
+  inputPressure = analogRead(pressureReading);
+  Serial.print(inputPressure);
   Serial.print("\t");
   
-  pressureDifference = (currentPressure - pumpThreshold); //positive if current is greater, negative if not
+  pressureDifference = (inputPressure - pressureThreshold); //positive if current is greater, negative if not
   Serial.print(pressureDifference);
   Serial.print("\n");
 
-  if(currentPressure < pumpThreshold){
-    //want to increase the pressure, therefore activates the fuel pump
+  if(pressureDifference < 0){
+    Serial.println("Fuel pump requires more pressure, increasing PDM Signal\n");
+    delay(100);
+    //want to increase the pressure, activate the fuel pump
     digitalWrite(pressureUnderLED, HIGH);
-    myPID.SetControllerDirection(DIRECT);
+    setPWM = 240;
+    Serial.println("Fuel pump PWM is now: " + setPWM);
+    delay(100);
+    analogWrite(pumpPWM, setPWM);
+    //myPID.SetControllerDirection(DIRECT);
   }
   else {
-    myPID.SetControllerDirection(IDLE); // set controller to idle until pressure evens out 
-    //not really doing much here 
-  }
+    //wants to let pressure coast until equal 
+    digitalWrite(pressureUnderLED, LOW);
+    setPWM = 60;
+//    myPID.SetControllerDirection(); // set controller to idle until pressure evens out 
+//    //not really doing much here 
+
+  Serial.print("\t");
+  Serial.print(setPWM);
+  Serial.println();
+  delay(100);
   myPID.Compute();
+}
+}
+void setPWMFrequency(int pin, int divisor) {
+  byte mode;
+  if(pin == 5 || pin == 6 || pin == 9 || pin == 10) {
+    switch(divisor) {
+      case 1: mode = 0x01; break;
+      case 8: mode = 0x02; break;
+      case 64: mode = 0x03; break;
+      case 256: mode = 0x04; break;
+      case 1024: mode = 0x05; break;
+      default: return;
+    }
+    if(pin == 5 || pin == 6) {
+      TCCR0B = TCCR0B & 0b11111000 | mode;
+    } else {
+      TCCR1B = TCCR1B & 0b11111000 | mode;
+    }
+  } else if(pin == 3 || pin == 11) {
+    switch(divisor) {
+      case 1: mode = 0x01; break;
+      case 8: mode = 0x02; break;
+      case 32: mode = 0x03; break;
+      case 64: mode = 0x04; break;
+      case 128: mode = 0x05; break;
+      case 256: mode = 0x06; break;
+      case 1024: mode = 0x07; break;
+      default: return;
+    }
+    TCCR2B = TCCR2B & 0b11111000 | mode;
+  }
 }
