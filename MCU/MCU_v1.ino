b3a18081a387578b7e9f6583f517ea21322e9932
Brent Champion <brent.champion@queensu.ca>
2019-04-09 23:49:10 -0400

    updates

--- MCU/MCU_v1.ino
+++ MCU/MCU_v1.ino
@@ -1,5 +1,4 @@
 //Arduino Code for Formula SAE Team MCU (Master Control Unit)
-
 /*
 // PIN DEFINITIONS 
 //Analog
@@ -29,6 +28,7 @@ PWM4. TPS_Speed
 
 */ 
 
+//----------------------------Pin Definitions--------------------------------
 //Analog Pins 
 const int TPS1Pin 	= 	A0;  //Pin of TPS1
 const int TPS2Pin 	= 	A1;  //Pin of TPS2
@@ -49,10 +49,25 @@ const int ETC_Flag	=	24;
 const int engNeut 	= 	25;
 const int upSol		=	26; //Signal to upshift solenoid
 const int downSol	= 	27; //Signal to downshift solenoid 
+//--------------------------------------------------------------------------
 
+//-------------------------------Delays-------------------------------------
+const int downShiftDelay 	= 	400; //from Q17 need to retune 
+const int upShiftDelay 		= 	200;
+//--------------------------------------------------------------------------
+
+//----------------------------Thresholds------------------------------------
+const int APPS1UpperThresh 	= 	0;
+const int APPS2UpperThresh 	=	0;
+const int TPS1UpperThresh 	= 	0;
+const int TPS2UpperThresh 	= 	0;
+
+const int APPS1LowerThresh 	= 	0;
+const int APPS2LowerThresh 	= 	0;
+const int TPS1LowerThresh 	= 	0;
+const int TPS2LowerThresh 	= 	0;
 
 //ETC Stuff
-/*
 //PID tunings
 const double Kp = 1;
 const double Ki = 0.00001;
@@ -80,24 +95,28 @@ int APPS1;
 int APPS2;
 int APPSdiff;
 
-PID myPID(&Input, &OutputTorque, &DesiredPosition, Kp, Ki, Kd, DIRECT);
+PID myPID(&Input, &OutputTorque, &DesiredPosition, Kp, Ki, Kd, DIRECT); //PID system for ETC
+//--------------------------------------------------------------------------
 
 //Function Protocols 
+void updateETB();
 void CANInterupt();
-int getRPM();
 void checkSignals();
+void manualMode();
+void manualDownShift();
+void manualUpShift();
+int getRPM();
 void blipDown();
-*/
+void neutralFind();
+//--------------------------------------------------------------------------
+
 void setup(){
 	Init();
 	Input = 0;
 	DesiredPosition = 0;
 	myPID.SetMode(AUTOMATIC);
-
-	pinMode(TPS_Dir, OUTPUT);
-	pinMode(TPS_Speed, OUTPUT);
-
 	myPID.SetSampleTime(10);
+	Serial.begin(9600);
 }
 
 void Init(){
@@ -112,7 +131,8 @@ void Init(){
 	
 	//PWM Pins
 	attachInterrupt(canInt, CANInterupt, ___); // need to write function for can intercept
-	
+	attachInterrupt(upBut, manualUpShift, __);
+	attachIntterupt(downBut, manualDownShift, __);
 	//Digital
 	pinMode(downBut, INPUT);
 	pinMode(upBut, INPUT);
@@ -122,11 +142,17 @@ void Init(){
 	pinMode(engNeut, OUTPUT);
 	pinMode(upSol, OUTPUT);
 	pinMode(downSol, OUTPUT);
+	pinMode(TPS_Dir, OUTPUT);
+	pinMode(TPS_Speed, OUTPUT);
 }
 
 void loop(){
 	checkSignals();
-	
+	updateETB();
+}
+
+//ETC Function
+void updateETB(){
 	//ETC Stuff -> Look into making this a function 
 	TPS1 = constrain(analogRead(TPS1Pin), TPS1Open, TPS1Close); // Range: 931 - 50
 	TPS2 = constrain(analogRead(TPS2Pin), TPS2Close, TPS2Open); // Range: 90 - 971
@@ -157,10 +183,48 @@ void loop(){
 	myPID.Compute();
 	//Update Write Function AFTER Compute
 	analogWrite(TPS_Speed, OutputTorque);
+}//updateETB()
+
+//Shifting Stuff
+void manualMode () {
+  //Upshift 
+  int time; 
+  if (!digitalRead(upBut)) {
+    Serial.println("Manual UP Shift Requested");
+    time = millis();
+    manualUpshift();
+    time = millis() - time;
+    Serial.print("Shift Time:  ");
+    Serial.println(time);
+    }    
+    
+    //Downshift
+    if(!digitalRead(downBut)) {
+      Serial.println("Manual DOWN Shift Requested");
+      time = millis();
+      
+      manualDownshift();
+      time = millis() - time;
+      Serial.print("Shift Time:  ");  
+      Serial.println(time);
+    }
+   
 }
 
-void CANInterupt(){
-//	
+void manualDownshift () {
+      digitalWrite(shiftCut, HIGH);
+      digitalWrite(downSol, HIGH);
+      delay(downShiftDelay);
+      digitalWrite(shiftCut, LOW); 
+      digitalWrite(downSol, LOW);  
+}
+
+void manualUpshift() {
+      digitalWrite(shiftCut, HIGH);
+      digitalWrite(upSol, HIGH);
+      delay(upShiftDelayTime);
+      digitalWrite(upSol, LOW); 
+      digitalWrite(shiftCut, LOW); 
 }
 
 int getRPM () {
@@ -193,9 +257,48 @@ void blipDown () {
    
 }
 
-void checkSignals(){
-	//Test each individual signal to make sure that it is plausible
-	
-
+void neutralFind() {
+  manualDownshift(); 
+  manualDownshift(); 
+  manualDownshift();
+      
+      //digitalWrite(shiftCutPin, HIGH);
+      digitalWrite(upSol, HIGH);
+      delay(upShiftDelayTime/2);
+      digitalWrite(upSol, LOW); 
+      //digitalWrite(shiftCutPin, LOW);
 }
 
+byte getGear () {
+  byte gear = 0;
+  return gear;
+}
+
+void CANInterupt(){
+//	Not sure what the CANInterupt is doing 
+}
+
+void checkSignals(){
+	//Test each individual signal to make sure that it is plausible
+	//Send out ETC Flag
+	//Not fully sure what to do here
+	
+	if(APPS1 > APPS1UpperThresh || APPS1 < APPS1LowerThresh){
+		digitalWrite(ETC_Flag, HIGH);
+	}
+	
+	if(APPS2 > APPS2UpperThresh || APPS2 < APPS2LowerThresh){
+		digitalWrite(ETC_Flag, HIGH);
+	}
+	
+	if(TPS1 > TPS1UpperThresh || TPS1 < TPS1LowerThresh){
+		digitalWrite(ETC_Flag, HIGH);
+	}
+	
+	if(TPS2 > TPS2UpperThresh || TPS2 < TPS2LowerThresh){
+		digitalWrite(ETC_Flag, HIGH);
+	}
+	
+}
+
+//--------------------------------------------------------------------------
