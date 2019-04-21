7899d3032d5385cec3ab6bb8bc10897e27dd8380
Brent Champion <brent.champion@queensu.ca>
2019-04-21 09:59:30 -0400

    yeet

--- Fuel_Controller/fuelControllerV1/fuelControllerV1.ino
+++ Fuel_Controller/fuelControllerV1/fuelControllerV1.ino
@@ -2,7 +2,7 @@
 
 const int pressureReading = A0; //Analog input from pressure sensor
 const int pumpPWM = 9; //PWM output from arduino
-const int pressureUnderLED = 3;
+
 
 int setPWM = 0;
 int currentPressure = 0;
@@ -11,11 +11,11 @@ double pressureDifference;
 //PID Control Inputs
 double outputPWM;
 double inputPressure;
-double pressureThreshold = 400;
+double pressureThreshold = 900;
 
 //PID Tunings
 const double kp = 1;
-const double ki = 0.00001;
+const double ki = 0.000001;
 const double kd = 0.00;
 
 PID myPID(&inputPressure, &outputPWM, &pressureThreshold, kp, ki, kd, DIRECT);
@@ -29,55 +29,53 @@ void setup(){
   myPID.SetMode(AUTOMATIC);
   myPID.SetSampleTime(2);
   
-  setPWM = 120; //resting PWM signal
+  setPWM = 0; //resting PWM signal
+ 
   analogWrite(pumpPWM, setPWM);
   
   pinMode(pressureReading, INPUT);
   pinMode(pumpPWM, OUTPUT);
 
-  pinMode(pressureUnderLED, OUTPUT);
-  digitalWrite(pressureUnderLED, LOW);
-
-  Serial.begin(9600);
-  Serial.print("Desired Pressure\t Current Pressure\t Pressure Difference\t OutputPWM\n");
+  Serial.begin(115200);
+  //Serial.print("Desired Pressure\t Current Pressure\t Pressure Difference\t OutputPWM\n");
 }
 
 void loop(){
-  Serial.print(pressureThreshold);
-  Serial.print("\t\t\t");
-  
-  inputPressure = analogRead(pressureReading);
-  Serial.print(inputPressure);
-  Serial.print("\t\t\t");
-  
-  pressureDifference = (pressureThreshold - inputPressure); //positive if current is greater, negative if not
-  Serial.print(pressureDifference);
-  Serial.print("\t\t\t");
-
-  if(pressureDifference < 0){
-      //want to increase the pressure, activate the fuel pump
-      digitalWrite(pressureUnderLED, HIGH);
-      setPWM = map(inputPressure, 0, 1024, 0, 255);
-      //Serial.println("Fuel pump PWM is now: " + setPWM);
-      myPID.Compute();
-      analogWrite(pumpPWM, outputPWM);
-      //myPID.SetControllerDirection(DIRECT);
-    }
-  
-  if(pressureDifference > 0){
-    //wants to let pressure coast until equal 
-    digitalWrite(pressureUnderLED, LOW);
-    setPWM = map(inputPressure, 0, 1024, 0, 255);
-    myPID.Compute();
-    analogWrite(pumpPWM, outputPWM);
-  }
-  digitalWrite(pressureUnderLED, LOW);
-  Serial.print("\t");
-  Serial.print(setPWM);
-  Serial.println();
-  delay(100);
+// // Serial.print(pressureThreshold);
+// // Serial.print("\t\t\t");
+//  
+//  inputPressure = analogRead(pressureReading);
+//  Serial.print(inputPressure);
+//  Serial.print("\t\t\t");
+//  
+//  pressureDifference = (pressureThreshold - inputPressure); //positive if current is greater, negative if not
+////  Serial.print(pressureDifference);
+////  Serial.print("\t\t\t");
+//
+//  if(pressureDifference < 0){
+//      //want to increase the pressure, activate the fuel pump
+//
+//      setPWM = map(inputPressure, 0, 1024, 0, 255);
+//      //Serial.println("Fuel pump PWM is now: " + setPWM);
+//      myPID.Compute();
+//      analogWrite(pumpPWM, outputPWM);
+//      //myPID.SetControllerDirection(DIRECT);
+//    }
+//  
+//  if(pressureDifference > 0){
+//    //wants to let pressure coast until equal 
+//
+//    setPWM = map(inputPressure, 0, 1024, 0, 255);
+//    myPID.Compute();
+//    analogWrite(pumpPWM, outputPWM);
+//  }
+// // digitalWrite(pressureUnderLED, LOW);
+//  Serial.print("\t");
+//  Serial.print(setPWM);
+//  Serial.println();
 
 }
+
 void setPWMFrequency(int pin, int divisor) {
   byte mode;
   if(pin == 5 || pin == 6 || pin == 9 || pin == 10) {
